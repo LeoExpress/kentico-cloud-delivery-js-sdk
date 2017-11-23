14ec23419eb66891044608582a12e892cec744bd
Martin Cajthaml <martin.cajthaml@symbio.agency>
2017-11-23 12:17:13 +0100

    removed bluebird

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,102 +1,105 @@
-import requestPromise from 'request-promise';
-import Promise from 'bluebird';
-import cheerio from 'cheerio';
+import requestPromise from 'request-promise'
+import cheerio from 'cheerio'
 
-export function getRawData(options) {
-    return Promise.map(options, item => requestPromise(item));
+export async function getRawData (options) {
+  const data = []
+  for (let i = 0; i < options.length; i++) {
+    data.push(await requestPromise(options[i]))
+  }
+  return data
 }
 
-export function getDeliveryUrlForTypes(projectID, isPreview) {
-    if (isPreview) {
-        return 'https://preview-deliver.kenticocloud.com/' + projectID + '/types';
-    } else {
-        return 'https://deliver.kenticocloud.com/' + projectID + '/types';
-    }
+export function getDeliveryUrlForTypes (projectID, isPreview) {
+  if (isPreview) {
+    return 'https://preview-deliver.kenticocloud.com/' + projectID + '/types'
+  } else {
+    return 'https://deliver.kenticocloud.com/' + projectID + '/types'
+  }
 }
 
-export function getDeliveryUrl(projectID, isPreview) {
-    if (isPreview) {
-        return 'https://preview-deliver.kenticocloud.com/' + projectID + '/items';
-    } else {
-        return 'https://deliver.kenticocloud.com/' + projectID + '/items';
-    }
+export function getDeliveryUrl (projectID, isPreview) {
+  if (isPreview) {
+    return 'https://preview-deliver.kenticocloud.com/' + projectID + '/items'
+  } else {
+    return 'https://deliver.kenticocloud.com/' + projectID + '/items'
+  }
 }
 
-export function getFullDeliveryUrls(params, projectID, previewKey, isPreview) {
-    const options = [];
+export function getFullDeliveryUrls (params, projectID, previewKey, isPreview) {
+  const options = []
 
-    if (isPreview && previewKey !== null) {
-        params.forEach((item) => {
-            options.push({
-                uri: getDeliveryUrl(projectID, isPreview) + item,
-                json: true,
-                headers: {
-                    Authorization: 'Bearer ' + previewKey
-                }
-            });
-        });
-    } else {
-        params.forEach((item) => {
-            options.push({
-                uri: getDeliveryUrl(projectID, isPreview) + item,
-                json: true
-            });
-        });
-    }
-    return options;
+  if (isPreview && previewKey !== null) {
+    params.forEach((item) => {
+      options.push({
+        uri: getDeliveryUrl(projectID, isPreview) + item,
+        json: true,
+        headers: {
+          Authorization: 'Bearer ' + previewKey
+        }
+      })
+    })
+  } else {
+    params.forEach((item) => {
+      options.push({
+        uri: getDeliveryUrl(projectID, isPreview) + item,
+        json: true
+      })
+    })
+  }
+  return options
 }
 
-export function getArrayValues(temp, assets, property) {
-    temp = [];
-    assets.value.forEach((item, index) => {
-        temp.push(item[property]);
-    });
+export function getArrayValues (temp, assets, property) {
+  temp = []
+  assets.value.forEach((item, index) => {
+    temp.push(item[property])
+  })
 
-    return temp;
+  return temp
 }
 
-export function getRichTextModularContent(data, modularContent) {
-    let text = data.value;
-    const $ = cheerio.load(text);
+export function getRichTextModularContent (data, modularContent) {
+  let text = data.value
+  const $ = cheerio.load(text)
 
-    data.modular_content.forEach((item, index) => {
-        $('object[data-codename="' + item + '"]').after('<script id="' + item + '">' + JSON.stringify(modularContent[item]) + '</script>');
-        text = $.html();
-    });
+  data.modular_content.forEach((item, index) => {
+    $('object[data-codename="' + item + '"]').after('<script id="' + item + '">' + JSON.stringify(modularContent[item]) + '</script>')
+    text = $.html()
+  })
 
-    return text.replace('<html><head></head><body>', '').replace('</body></html>', '');
+  return text.replace('<html><head></head><body>', '').replace('</body></html>', '')
 }
 
-export const hasOwnProperty = Object.prototype.hasOwnProperty;
+export const hasOwnProperty = Object.prototype.hasOwnProperty
 
-export function isEmptyObject(obj) {
+export function isEmptyObject (obj) {
 
-    // null and undefined are "empty"
-    if (obj == null) return true;
+  // null and undefined are "empty"
+  if (obj == null) return true
 
-    // Assume if it has a length property with a non-zero value
-    // that that property is correct.
-    if (obj.length > 0) return false;
-    if (obj.length === 0) return true;
+  // Assume if it has a length property with a non-zero value
+  // that that property is correct.
+  if (obj.length > 0) return false
+  if (obj.length === 0) return true
 
-    // If it isn't an object at this point
-    // it is empty, but it can't be anything *but* empty
-    // Is it empty?  Depends on your application.
-    if (typeof obj !== "object") return true;
+  // If it isn't an object at this point
+  // it is empty, but it can't be anything *but* empty
+  // Is it empty?  Depends on your application.
+  if (typeof obj !== 'object') return true
 
-    // Otherwise, does it have any properties of its own?
-    // Note that this doesn't handle
-    // toString and valueOf enumeration bugs in IE < 9
-    for (var key in obj) {
-        if (hasOwnProperty.call(obj, key)) return false;
-    }
+  // Otherwise, does it have any properties of its own?
+  // Note that this doesn't handle
+  // toString and valueOf enumeration bugs in IE < 9
+  for (var key in obj) {
+    if (hasOwnProperty.call(obj, key)) return false
+  }
 
-    return true;
+  return true
 }
 
-export function isObject(val) {
-    if (val === null) {
-        return false;
-    }
-    return ( (typeof val === 'function') || (typeof val === 'object') ) && !(val instanceof Array);
+export function isObject (val) {
+  if (val === null) {
+    return false
+  }
+  return ( (typeof val === 'function') || (typeof val === 'object') ) && !(val instanceof Array)
 }
